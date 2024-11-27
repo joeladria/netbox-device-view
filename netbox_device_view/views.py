b666ecac70a88f2ef6fa61a9aa9eeeae1d8439f8
Joel Adria <joela@momentfactory.com>
2024-11-27 10:07:35 -0500

    port click options, vlan css classes, host & label improvements

--- netbox_device_view/views.py
+++ netbox_device_view/views.py
@@ -53,6 +53,7 @@ class DeviceDeviceView(generic.ObjectView):
             "cable_colors": request.GET.get("cable_colors", "off"),
             "port_type": request.GET.get("port_type", "status"),
             "display_size": request.GET.get("display_size", "small"),
+            "link_type": request.GET.get("link_type", "trace"),
             "something_else": request.GET.get("something_else", "off"),
         }
 
