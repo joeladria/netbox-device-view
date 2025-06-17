a664e4b5996f4d6bb661dac9c7d38cf6b74ca6d8
Joel Adria <joela@momentfactory.com>
2025-06-17 09:32:58 -0400

    host shortname

--- netbox_device_view/templatetags/device_view_filters.py
+++ netbox_device_view/templatetags/device_view_filters.py
@@ -2,11 +2,11 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='truncate_hostname_prefix')
-def truncate_hostname_prefix(value):
+@register.filter(name='get_hostname_last_segment')
+def get_hostname_last_segment(value):
     if not isinstance(value, str):
         return value
-    parts = value.split('-', 1)
-    if len(parts) > 1:
-        return parts[1]  # Return the part after the first dash
-    return value     # Return the original string if no dash is found
+    parts = value.split('-')
+    if parts: # Check if parts is not empty
+        return parts[-1]  # Return the last segment
+    return value     # Return the original string if split results in empty or no dash
