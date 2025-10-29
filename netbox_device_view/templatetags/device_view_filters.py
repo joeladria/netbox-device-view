75b4bda98ed821748d39b57ef05b14fdc9c64f3c
Joel Adria <joela@momentfactory.com>
2025-10-29 10:33:31 -0400

    last segment filter fix

--- netbox_device_view/templatetags/device_view_filters.py
+++ netbox_device_view/templatetags/device_view_filters.py
@@ -7,6 +7,6 @@ def get_hostname_last_segment(value):
     if not isinstance(value, str):
         return value
     parts = value.split('-')
-    if parts: # Check if parts is not empty
-        return parts[-1]  # Return the last segment
-    return value     # Return the original string if split results in empty or no dash
+    if parts: 
+        return parts[-1] 
+    return value
